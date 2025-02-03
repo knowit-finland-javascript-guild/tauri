a480fe9f6e38c1b7040e32db0223e88da85b7646
Aleksi Alhola <aleksi.alhola@valmetpartners.com>
2025-02-03 09:17:21 +0200

    step 2 - add mock data example

--- README.md
+++ README.md
@@ -35,3 +35,23 @@
 - Select preferred project framework
 - Install node dependencies
 - Run command ```npm run tauri dev``` to start the example app
+
+## Step 2
+
+### Frontend
+
+Developing the frontend does not differ much from regular UI development. There are some caveats when implementing some features that work out of the box in web (drag and drop for example), but we do not dive too deeply into those here.
+
+- Modify frontend code to include mock data for system info
+- Display the mock data
+
+```ts
+interface SystemInfo {
+  system_name?: string;
+  kernel_version?: string;
+  os_version?: string;
+  host_name?: string;
+  memory_usage: number;
+  cpu_usage: number;
+}
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,34 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+
+interface SystemInfo {
+  system_name?: string;
+  kernel_version?: string;
+  os_version?: string;
+  host_name?: string;
+  memory_usage: number;
+  cpu_usage: number;
+}
+
+const exampleData: SystemInfo = {
+  system_name: "Windows",
+  kernel_version: "1",
+  os_version: "1",
+  host_name: "Knowit",
+  memory_usage: 1000,
+  cpu_usage: 50,
+};
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+  const [systemInfo, setSystemInfo] = useState(exampleData);
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+    <div>
+      <p>System name: {systemInfo.system_name}</p>
+      <p>Kernel version: {systemInfo.kernel_version}</p>
+      <p>OS version: {systemInfo.os_version}</p>
+      <p>Host name: {systemInfo.host_name}</p>
+      <p>Memory usage: {systemInfo.memory_usage}B</p>
+      <p>CPU usage: {systemInfo.cpu_usage}%</p>
+    </div>
   );
 }
 
